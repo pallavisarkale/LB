2f1fa9d2a024365ceb3ef4a8416ea483f631c1dc
Pallavi Sarkale <pallavisarkale2110@gmail.com>
2022-04-13 15:01:14 +0530

    Addition of two Number using function

---
+++ JavaProgram/AddFunction.java
@@ -0,0 +1,17 @@
+package LB.JavaProgram;
+ 
+public class AddFunction{
+    public static int Add(int iValue1,int iValue2)
+    {
+        if(iValue1<0)
+        {
+            iValue1=-iValue1;
+        }
+        if(iValue2<0)
+        {
+            iValue2=-iValue2;
+        }
+        int iAns=iValue1+iValue2;
+        return iAns;
+    }
+}
---
+++ JavaProgram/Addition.java
@@ -0,0 +1,27 @@
+package LB.JavaProgram;
+
+import LB.JavaProgram.AddFunction;
+import java.util.Scanner;
+
+public class Addition {
+
+    
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int iNo1=0;
+        int iNo2=0;
+        int iNo3=0;
+
+        System.out.println("Enter first Number: ");
+        iNo1=s.nextInt();
+
+        System.out.println("Enter second Number: ");
+        iNo2=s.nextInt();
+
+        iNo3=AddFunction.Add(iNo1,iNo2);
+
+        System.out.println("Addition is :" +iNo3);
+
+    }
+    
+}
